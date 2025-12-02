12687241f71020cc3f00dd8fa60f6c476ba19f5a
leeedohyun <ldh19738@gmail.com>
2025-12-02 15:36:20 +0900

    feat: 결제 모듈 생성

--- payment-mock-server/src/main/java/com/example/demo/PaymentMockServerApplication.java
+++ payment-mock-server/src/main/java/com/example/demo/PaymentMockServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class PaymentMockServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMockServerApplication.class, args);
+    }
 
 }
